193899085802d54821e65db2d93e272cebc9aed8
sharina-ya <uliasarina7@gmail.com>
2025-12-03 02:18:27 +1000

    Update Cheatmenu.cs

--- Cheatmenu.cs
+++ Cheatmenu.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Reflection;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -27,6 +28,11 @@ namespace StardewValley.Menus
 			this.closeButton = new ClickableTextureComponent(new Rectangle(this.xPositionOnScreen + this.width + 10, this.yPositionOnScreen - 80, 40, 40), Game1.mouseCursors, new Rectangle(337, 494, 12, 12), 4f, false);
 			this.immortalButton = new ClickableTextureComponent(new Rectangle(num + num2 * 2, y, 150, 40), Game1.mouseCursors, new Rectangle(0, 256, 64, 64), 0.8f, false);
 			this.timeButton = new ClickableTextureComponent(new Rectangle(num, y + 100, 150, 40), Game1.mouseCursors, new Rectangle(0, 256, 64, 64), 0.8f, false);
+			int speedY = y + 200;
+			this.speedUpButton = new ClickableTextureComponent(new Rectangle(num, speedY, 150, 40), Game1.mouseCursors, new Rectangle(0, 256, 64, 64), 0.8f, false);
+			this.speedDownButton = new ClickableTextureComponent(new Rectangle(num + 170, speedY, 150, 40), Game1.mouseCursors, new Rectangle(0, 256, 64, 64), 0.8f, false);
+			this.speedResetButton = new ClickableTextureComponent(new Rectangle(num + 340, speedY, 150, 40), Game1.mouseCursors, new Rectangle(0, 256, 64, 64), 0.8f, false);
+			CheatMenu.InitializeOriginalSpeed();
 		}
 
 		// Token: 0x06002984 RID: 10628
@@ -67,6 +73,15 @@ namespace StardewValley.Menus
 			string timeLabel = CheatMenu.TimeFrozen ? "On" : "Off";
 			Vector2 timeSize = Game1.dialogueFont.MeasureString(timeLabel);
 			Utility.drawTextWithShadow(b, timeLabel, Game1.smallFont, new Vector2((float)(this.timeButton.bounds.X + this.timeButton.bounds.Width / 2) - timeSize.X / 2f, (float)this.timeButton.bounds.Y), Color.White, 1f, -1f, -1, -1, 1f, 3);
+			Utility.drawTextWithShadow(b, "Speed:", Game1.smallFont, new Vector2((float)(this.xPositionOnScreen + 150), (float)(this.yPositionOnScreen + 90 + 200 + 10)), Color.White, 1f, -1f, -1, -1, 1f, 3);
+			IClickableMenu.drawTextureBox(b, Game1.mouseCursors, new Rectangle(432, 439, 9, 9), this.speedUpButton.bounds.X, this.speedUpButton.bounds.Y, this.speedUpButton.bounds.Width, this.speedUpButton.bounds.Height, this.speedUpButton.containsPoint(Game1.getMouseX(), Game1.getMouseY()) ? Color.Wheat : Color.White, 4f, false);
+			Utility.drawTextWithShadow(b, "Faster", Game1.smallFont, new Vector2((float)(this.speedUpButton.bounds.X + 10), (float)(this.speedUpButton.bounds.Y + 8)), Color.White, 1f, -1f, -1, -1, 1f, 3);
+			IClickableMenu.drawTextureBox(b, Game1.mouseCursors, new Rectangle(432, 439, 9, 9), this.speedDownButton.bounds.X, this.speedDownButton.bounds.Y, this.speedDownButton.bounds.Width, this.speedDownButton.bounds.Height, this.speedDownButton.containsPoint(Game1.getMouseX(), Game1.getMouseY()) ? Color.Wheat : Color.White, 4f, false);
+			Utility.drawTextWithShadow(b, "Slower", Game1.smallFont, new Vector2((float)(this.speedDownButton.bounds.X + 10), (float)(this.speedDownButton.bounds.Y + 8)), Color.White, 1f, -1f, -1, -1, 1f, 3);
+			IClickableMenu.drawTextureBox(b, Game1.mouseCursors, new Rectangle(432, 439, 9, 9), this.speedResetButton.bounds.X, this.speedResetButton.bounds.Y, this.speedResetButton.bounds.Width, this.speedResetButton.bounds.Height, this.speedResetButton.containsPoint(Game1.getMouseX(), Game1.getMouseY()) ? Color.Wheat : Color.White, 4f, false);
+			Utility.drawTextWithShadow(b, "Normal", Game1.smallFont, new Vector2((float)(this.speedResetButton.bounds.X + 10), (float)(this.speedResetButton.bounds.Y + 8)), Color.White, 1f, -1f, -1, -1, 1f, 3);
+			string speedText = string.Format("Current: x{0:F1}", CheatMenu.SpeedMultiplier);
+			Utility.drawTextWithShadow(b, speedText, Game1.smallFont, new Vector2((float)this.speedUpButton.bounds.X, (float)(this.speedUpButton.bounds.Y + this.speedUpButton.bounds.Height + 10)), Color.LightGreen, 1f, -1f, -1, -1, 1f, 3);
 			this.closeButton.draw(b);
 			base.draw(b);
 			base.drawMouse(b);
@@ -134,6 +149,32 @@ namespace StardewValley.Menus
 				CheatMenu.FrozenTimeOfDay = Game1.timeOfDay;
 				Game1.addHUDMessage(new HUDMessage("Time: stopped", 2));
 			}
+			if (this.speedUpButton.containsPoint(x, y))
+			{
+				CheatMenu.SpeedMultiplier += 0.5f;
+				if (CheatMenu.SpeedMultiplier > 5f)
+				{
+					CheatMenu.SpeedMultiplier = 5f;
+				}
+				CheatMenu.ApplySpeed();
+				Game1.addHUDMessage(new HUDMessage(string.Format("Speed: x{0:F1}", CheatMenu.SpeedMultiplier), 2));
+			}
+			if (this.speedDownButton.containsPoint(x, y))
+			{
+				CheatMenu.SpeedMultiplier -= 0.5f;
+				if (CheatMenu.SpeedMultiplier < 0.2f)
+				{
+					CheatMenu.SpeedMultiplier = 0.2f;
+				}
+				CheatMenu.ApplySpeed();
+				Game1.addHUDMessage(new HUDMessage(string.Format("Speed: x{0:F1}", CheatMenu.SpeedMultiplier), 2));
+			}
+			if (this.speedResetButton.containsPoint(x, y))
+			{
+				CheatMenu.SpeedMultiplier = 1f;
+				CheatMenu.ApplySpeed();
+				Game1.addHUDMessage(new HUDMessage("Speed: normal", 2));
+			}
 		}
 
 		// Token: 0x06002988 RID: 10632
@@ -173,6 +214,136 @@ namespace StardewValley.Menus
 		{
 			base.update(time);
 			this.moneyTextBox.Update();
+			CheatMenu.UpdateSpeedContinuously();
+		}
+
+		// Token: 0x06002F6C RID: 12140
+		static CheatMenu()
+		{
+			CheatMenu.SpeedMultiplier = 1f;
+			CheatMenu.TimeFrozen = false;
+			CheatMenu.FrozenTimeOfDay = 600;
+		}
+
+		// Token: 0x06003128 RID: 12584
+		private static void InitializeOriginalSpeed()
+		{
+			if (CheatMenu.OriginalBaseSpeed < 0f && Game1.player != null)
+			{
+				CheatMenu.OriginalBaseSpeed = (float)Game1.player.speed;
+				CheatMenu.OriginalAddedSpeed = (float)Game1.player.addedSpeed;
+			}
+		}
+
+		// Token: 0x06003129 RID: 12585
+		private static void ApplySpeed()
+		{
+			try
+			{
+				if (CheatMenu.OriginalBaseSpeed < 0f)
+				{
+					CheatMenu.InitializeOriginalSpeed();
+				}
+				int newSpeed = (int)(CheatMenu.OriginalBaseSpeed * CheatMenu.SpeedMultiplier);
+				int newAddedSpeed = (int)(CheatMenu.OriginalAddedSpeed * CheatMenu.SpeedMultiplier);
+				if (newSpeed < 0)
+				{
+					newSpeed = 0;
+				}
+				if (newAddedSpeed < 0)
+				{
+					newAddedSpeed = 0;
+				}
+				if (newSpeed > 20)
+				{
+					newSpeed = 20;
+				}
+				if (newAddedSpeed > 20)
+				{
+					newAddedSpeed = 20;
+				}
+				if (Game1.player != null)
+				{
+					Game1.player.speed = newSpeed;
+					Game1.player.addedSpeed = newAddedSpeed;
+				}
+			}
+			catch (Exception)
+			{
+			}
+		}
+
+		// Token: 0x0600312A RID: 12586
+		public static void UpdateSpeedContinuously()
+		{
+			try
+			{
+				if (Game1.player != null)
+				{
+					if (CheatMenu.SpeedMultiplier != 1f)
+					{
+						if (CheatMenu.OriginalBaseSpeed < 0f)
+						{
+							CheatMenu.InitializeOriginalSpeed();
+						}
+						int targetSpeed = (int)(CheatMenu.OriginalBaseSpeed * CheatMenu.SpeedMultiplier);
+						int targetAddedSpeed = (int)(CheatMenu.OriginalAddedSpeed * CheatMenu.SpeedMultiplier);
+						if (targetSpeed < 0)
+						{
+							targetSpeed = 0;
+						}
+						if (targetAddedSpeed < 0)
+						{
+							targetAddedSpeed = 0;
+						}
+						if (targetSpeed > 20)
+						{
+							targetSpeed = 20;
+						}
+						if (targetAddedSpeed > 20)
+						{
+							targetAddedSpeed = 20;
+						}
+						if (Game1.player.speed == targetSpeed && Game1.player.addedSpeed == targetAddedSpeed)
+						{
+							goto IL_174;
+						}
+						Game1.player.speed = targetSpeed;
+						Game1.player.addedSpeed = targetAddedSpeed;
+						try
+						{
+							Type typeFromHandle = typeof(Character);
+							FieldInfo netSpeedField = typeFromHandle.GetField("netSpeed", BindingFlags.Instance | BindingFlags.NonPublic);
+							FieldInfo netAddedSpeedField = typeFromHandle.GetField("netAddedSpeed", BindingFlags.Instance | BindingFlags.NonPublic);
+							if (netSpeedField != null && netAddedSpeedField != null)
+							{
+								object netSpeedObj = netSpeedField.GetValue(Game1.player);
+								object netAddedSpeedObj = netAddedSpeedField.GetValue(Game1.player);
+								PropertyInfo valueProperty = netSpeedObj.GetType().GetProperty("Value");
+								if (valueProperty != null)
+								{
+									valueProperty.SetValue(netSpeedObj, targetSpeed);
+									valueProperty.SetValue(netAddedSpeedObj, targetAddedSpeed);
+								}
+							}
+							goto IL_174;
+						}
+						catch
+						{
+							goto IL_174;
+						}
+					}
+					if (CheatMenu.OriginalBaseSpeed >= 0f && (Game1.player.speed != (int)CheatMenu.OriginalBaseSpeed || Game1.player.addedSpeed != (int)CheatMenu.OriginalAddedSpeed))
+					{
+						Game1.player.speed = (int)CheatMenu.OriginalBaseSpeed;
+						Game1.player.addedSpeed = (int)CheatMenu.OriginalAddedSpeed;
+					}
+				}
+				IL_174:;
+			}
+			catch (Exception)
+			{
+			}
 		}
 
 		// Token: 0x040023C8 RID: 9160
@@ -197,9 +368,27 @@ namespace StardewValley.Menus
 		private ClickableTextureComponent timeButton;
 
 		// Token: 0x04002A8F RID: 10895
-		public static bool TimeFrozen = false;
+		public static bool TimeFrozen;
 
 		// Token: 0x04002A90 RID: 10896
-		public static int FrozenTimeOfDay = 600;
+		public static int FrozenTimeOfDay;
+
+		// Token: 0x04002C48 RID: 11336
+		public static float SpeedMultiplier;
+
+		// Token: 0x04002C50 RID: 11344
+		private ClickableTextureComponent speedUpButton;
+
+		// Token: 0x04002C51 RID: 11345
+		private ClickableTextureComponent speedDownButton;
+
+		// Token: 0x04002C52 RID: 11346
+		private ClickableTextureComponent speedResetButton;
+
+		// Token: 0x04002C57 RID: 11351
+		private static float OriginalBaseSpeed = -1f;
+
+		// Token: 0x04002C58 RID: 11352
+		private static float OriginalAddedSpeed = -1f;
 	}
 }
